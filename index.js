bc8025040ec469adc1a1c40d1a6fcf8b953a8286
Yimura <andreas.maerten@scarlet.be>
2020-10-15 13:01:40 +0200

    Index: Use better shorthand to detect if the reaction is from the bot

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ export default class ReactionInterface extends BaseModule {
     }
 
     _onReaction(reactionType, messageReaction, user) {
-        if (user.id == this._m.user.id) return;
+        // Wether the bot did the reaction
+        if (messageReaction.me) return;
 
         const messageId = messageReaction.message.id;
 
